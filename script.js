e284905b751be571718ddc2fbd7da2e795b64cb0
Joseph Martin <josephmartin721@yahoo.com>
2021-02-08 19:41:59 -0600

    Question props functional, password generation is not.

--- script.js
+++ script.js
@@ -14,26 +14,51 @@ generateBtn.addEventListener("click", writePassword);
 
 function generatePassword(){
   
-  var uppercaseArray = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
+  var uppercasesArray = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
   
-  var lowercaseArray = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o",  "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+  var lowercasesArray = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o",  "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
   
   var numberArray = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
   
   var specialArray = ["@", "#", "$", "%", "^", "&", "*", "(", ")", "-", "_", "=", "+"];
+  
+  var resultArray = [];
 
   var passwordLength = prompt("Select how many characters are in your password 1-8");
 
-  var lowerCases = confirm("Does your password contain lowercases?");
+  var lowercases = confirm("Does your password contain lowercases?");
 
-  var upperCases = confirm("Does your password contain uppercases?");
+  var uppercases = confirm("Does your password contain uppercases?");
 
   var special = confirm("Does your password contain specail characters?");
 
-  var numbers = confirm("Does your password contain numbers?");
+  var number = confirm("Does your password contain numbers?");
 
-  var minimumCount = 0;
+  var minimumCount = 1;
 
   var maximumCount = 8;
+
+  if (uppercases){
+    resultArray = resultArray.concat(uppercasesArray);
+  }
+
+  if (lowercases){
+    resultArray = resultArray.concat(lowercasesArray)
+  }
   
-}
+  if (number){
+    resultArray = resultArray.concat(numberArray)
+  }
+  
+  if (special){
+    resultArray = resultArray.concat(specialArray)
+  }
+  console.log(resultArray)
+
+  for (var i = 0; i < numCharacter; i++) {
+      
+    userArray.push (resultArray[Math.floor(Math.random() * resultArray.length)]); 
+    }
+  
+    return userArray.join("") ;
+  }
